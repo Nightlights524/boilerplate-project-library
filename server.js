b0f27ea5f97d83bf823ded22ffa1f58740e6fec8
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-28 20:54:44 -0600

    Edit Port

--- server.js
+++ server.js
@@ -38,20 +38,22 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 1500);
-  }
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log("Listening on port " + port);
+  // if(process.env.NODE_ENV==='test') {
+  //   console.log('Running Tests...');
+  //   setTimeout(function () {
+  //     try {
+  //       runner.run();
+  //     } catch(e) {
+  //       let error = e;
+  //         console.log('Tests are not valid:');
+  //         console.log(error);
+  //     }
+  //   }, 1500);
+  // }
 });
 
 module.exports = app; //for unit/functional testing
