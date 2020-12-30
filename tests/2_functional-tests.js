1449e1d3ba22407858d2b03337f8a13687d641b5
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-29 18:00:38 -0600

    >Delete FreeCodeCamp comments

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       
-*/
-
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
@@ -15,26 +7,6 @@ const { MongoNetworkError } = require('mongodb');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
-  /*
-  * ----[EXAMPLE TEST]----
-  * Each test should completely test the response of the API end-point including response status code!
-  */
-  // test('#example Test GET /api/books', function(done){
-  //    chai.request(server)
-  //     .get('/api/books')
-  //     .end(function(err, res){
-  //       assert.equal(res.status, 200);
-  //       assert.isArray(res.body, 'response should be an array');
-  //       assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-  //       assert.property(res.body[0], 'title', 'Books in array should contain title');
-  //       assert.property(res.body[0], '_id', 'Books in array should contain _id');
-  //       done();
-  //     });
-  // });
-  /*
-  * ----[END of EXAMPLE TEST]----
-  */
 
   suite('Routing tests', function() {
 
